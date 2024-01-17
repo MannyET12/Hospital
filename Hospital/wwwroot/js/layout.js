9e51a1992594ae14a0c13de30e475d8b2ca57e54
MannyET <emmanueldtsegula@gmail.com>
2024-01-17 05:00:37 +0200

    updated sidebar
    sidebar css updated

---
+++ Hospital/wwwroot/js/layout.js
@@ -0,0 +1,10 @@
+﻿function status(element) { // Remove the "active" class from all li elements
+    // Remove the "active" class from all li elements
+    var listItems = document.querySelectorAll('#sidenav-list li');
+
+    listItems.forEach(function (li) {
+        li.classList.remove('active');
+    });
+
+    element.parentElement.classList.add('active');
+}
